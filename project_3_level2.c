8ec7df76245abe2b32695fc3afff205a2ac11d28
Başar Çelebi <75533431+celebibasar@users.noreply.github.com>
2022-05-29 09:13:43 +0300

    Add files via upload

---
+++ project_3_level2.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <math.h>
+#include <ctype.h>
+#define n 3 // Matrix Capacity
+
+int main()
+{
+    int i, j, k;
+    double x;
+    double a[n][n + 1];
+    double res[n];
+    FILE* in;
+    FILE* out;
+    in = fopen("input.txt", "r");
+    out = fopen("output.txt", "w");
+
+    if (in == NULL || out == NULL)
+    {
+        printf("File couldn't be opened\n");
+        return -1;
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n + 1; j++)
+        {
+            fscanf(in, "%lf", &a[i][j]);
+        }
+    }
+
+    for (i = 0; i < n; i++) // Initialization of the resulting array with zeros
+    {
+        res[i] = 0;
+    }
+
+    for (k = 0; k < n; k++)
+    {
+        x = fabs(a[k][k]);
+        i = k;
+        for (j = k + 1; j < n; j++)
+        {
+            if (fabs(a[j][k]) > x)
+            {
+                i = j;
+                x = fabs(a[j][k]);
+            }
+        }
+
+        if (x == 0) // Division by zero
+        {
+            printf("The system of linear equations has no solutions.\n");
+            return -1;
+        }
+
+        if (i != k) //Change lines
+        {
+            for (j = k; j < n + 1; j++)
+            {
+                x = a[k][j];
+                a[k][j] = a[i][j];
+                a[i][j] = x;
+            }
+        }
+
+        for (i = k + 1; i < n; i++) //Let's transform the strings.
+        {
+            x = a[i][k] / a[k][k];      //Row Factor
+            for (j = k; j < n + 1; j++) //Converting the elements of a string.
+            {
+                a[i][j] -= x * a[k][j];
+            }
+        }
+    }
+
+    for (i = n - 1; i >= 0; i--)
+    {
+        x = a[i][n];
+        for (j = n - 1; j >= i; j--)
+        {
+            if (j > i)
+            {
+                x -= res[j] * a[i][j];
+            }
+            if (j == i)
+            {
+                x /= a[i][j];
+            }
+        }
+        res[i] = x;
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        fprintf(out, "x%d = %lf\n", i + 1, res[i]);
+    }
+}
